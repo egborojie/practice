bc57a544b315025bbc7128002d0780ec76e78b3a
ojie <egborojie337@gmail.com>
2022-12-30 19:14:42 +0100

    diamond shape

---
+++ cProgramming/Hellowold/diamond.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main- write a code that prints a diamond shape.
+ */
+int main ()
+{
+	printf("\n\t\t\t*\t\t\t\n");
+	printf("\n\t\t*\t\t*\t\t\n");
+	printf("\n\t*\t\t\t\t*\t\n");
+	printf("\n*\t\t\t\t\t\t*\n");
+	printf("\n\t*\t\t\t\t*\t\n");
+	printf("\n\t\t*\t\t*\t\t\n");
+	printf("\n\t\t\t*\t\t\t\n");
+	return 0;
+}
